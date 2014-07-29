2c3b1ca822d5a04325fa75d28a5a5321c48de739
Jordi Esteve <jesteve@zikzakmedia.com>
2014-07-29 21:10:19 +0200

    Compute visible discount when product or quantity changes. Compute visible discount only for out invoices.

--- invoice.py
+++ invoice.py
@@ -2,6 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from decimal import Decimal
+from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.transaction import Transaction
 
@@ -12,19 +13,49 @@ __metaclass__ = PoolMeta
 class InvoiceLine:
     __name__ = 'account.invoice.line'
 
-    def update_prices(self):
-        res = super(InvoiceLine, self).update_prices()
-        if hasattr(self, 'product') and hasattr(self, 'invoice'):
-            with Transaction().set_context({
-                    'price_list': self.invoice.party.sale_price_list,
-                    'customer': self.invoice.party.id,
-                    }):
-                Product = Pool().get('product.product')
-                unit_price = Product.get_sale_price([self.product],
+    def update_prices_visible_discount(self):
+        if not hasattr(self, 'product') or not hasattr(self, 'invoice'):
+            return {}
+        with Transaction().set_context({
+                'price_list': self.invoice.party.sale_price_list,
+                'customer': self.invoice.party.id,
+                }):
+            Product = Pool().get('product.product')
+            gross_unit_price = self.gross_unit_price
+            discount = Decimal(0)
+            unit_price = Product.get_sale_price([self.product],
                     self.quantity or 0)[self.product.id]
-                if unit_price:
-                    res['unit_price'] = unit_price.quantize(
-                        Decimal(1) / 10 ** self.__class__.unit_price.digits[1])
-                    res['discount'] = 1 - (res['unit_price'] /
-                        res['gross_unit_price'])
+            if unit_price:
+                unit_price_digits = self.__class__.unit_price.digits[1]
+                discount_digits = self.__class__.discount.digits[1]
+                unit_price = unit_price.quantize(
+                    Decimal(str(10.0 ** -unit_price_digits)))
+                discount = 1 - (unit_price / gross_unit_price)
+                discount = discount.quantize(
+                    Decimal(str(10.0 ** -discount_digits)))
+            return {
+                'gross_unit_price': gross_unit_price,
+                'discount': discount,
+                'unit_price': unit_price,
+                }
+
+    def on_change_product(self):
+        res = super(InvoiceLine, self).on_change_product()
+        invoice_type = self.invoice_type or self.invoice and self.invoice.type
+        if (invoice_type in ('out_invoice', 'out_credit_note') and
+                'gross_unit_price' in res):
+            self.gross_unit_price = res['gross_unit_price']
+            res.update(self.update_prices_visible_discount())
+        return res
+
+    @fields.depends('product', 'quantity', 'invoice_type', 'party', 'invoice',
+        '_parent_invoice.type', '_parent_invoice.party')
+    def on_change_quantity(self):
+        res = {}
+        invoice_type = self.invoice_type or self.invoice and self.invoice.type
+        if invoice_type in ('out_invoice', 'out_credit_note'):
+            res = super(InvoiceLine, self).on_change_product()
+            if 'gross_unit_price' in res:
+                self.gross_unit_price = res['gross_unit_price']
+                res.update(self.update_prices_visible_discount())
         return res
--- sale.py
+++ sale.py
@@ -11,21 +11,36 @@ __metaclass__ = PoolMeta
 class SaleLine:
     __name__ = 'sale.line'
 
-    def update_prices(self):
-        if hasattr(self, 'product') and self.discount == Decimal(0):
-            self.discount = Decimal(0)
-            res = super(SaleLine, self).update_prices()
-            Product = Pool().get('product.product')
-            gross_unit_price = Product.get_sale_price([self.product],
-                    self.quantity or 0)[self.product.id]
-            if gross_unit_price:
-                unit_price_digits = self.__class__.gross_unit_price.digits[1]
-                discount_digits = self.__class__.discount.digits[1]
-                res['gross_unit_price'] = gross_unit_price.quantize(
-                    Decimal(str(10.0 ** -unit_price_digits)))
-                discount = 1 - (res['unit_price'] / res['gross_unit_price'])
-                res['discount'] = discount.quantize(
-                    Decimal(str(10.0 ** -discount_digits)))
-        else:
-            res = super(SaleLine, self).update_prices()
+    def update_prices_visible_discount(self):
+        Product = Pool().get('product.product')
+        unit_price = self.gross_unit_price
+        discount = Decimal(0)
+        gross_unit_price = Product.get_sale_price([self.product],
+                self.quantity or 0)[self.product.id]
+        if gross_unit_price:
+            unit_price_digits = self.__class__.gross_unit_price.digits[1]
+            discount_digits = self.__class__.discount.digits[1]
+            gross_unit_price = gross_unit_price.quantize(
+                Decimal(str(10.0 ** -unit_price_digits)))
+            discount = 1 - (unit_price / gross_unit_price)
+            discount = discount.quantize(
+                Decimal(str(10.0 ** -discount_digits)))
+        return {
+            'gross_unit_price': gross_unit_price,
+            'discount': discount,
+            'unit_price': unit_price,
+            }
+
+    def on_change_product(self):
+        res = super(SaleLine, self).on_change_product()
+        if 'gross_unit_price' in res:
+            self.gross_unit_price = res['gross_unit_price']
+            res.update(self.update_prices_visible_discount())
+        return res
+
+    def on_change_quantity(self):
+        res = super(SaleLine, self).on_change_quantity()
+        if 'gross_unit_price' in res:
+            self.gross_unit_price = res['gross_unit_price']
+            res.update(self.update_prices_visible_discount())
         return res
