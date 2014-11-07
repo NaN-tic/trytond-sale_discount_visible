309d0ff1b38cdab96fafb48e4f22e2c26186d6a1
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:37:32 +0100

    Cleanup tests

--- tests/test_sale_discount_visible.py
+++ tests/test_sale_discount_visible.py
@@ -2,31 +2,19 @@
 # This file is part of the sale_discount_visible module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
 
 class SaleDiscountVisibleTestCase(unittest.TestCase):
-    '''
-    Test Sale Discount Visible module.
-    '''
+    'Test Sale Discount Visible module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_discount_visible')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
